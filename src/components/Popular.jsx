da45fd681cfb82cf3eef69ec5cf4e958eeee0a68
Keiran Brown <keiranbrown1301@gmail.com>
2022-06-29 17:59:02 +0900

    added api call to recipe site

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,13 @@
 function Popular() {
+
+  const getPopular = async () => {
+    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+    const data = await api.json();
+    console.log(data)
+  }
+
+
   return (
     <div>popular</div>
   )
