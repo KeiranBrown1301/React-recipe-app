9d91289d807b955f5c77129319a85ce9c66334b6
Keiran Brown <keiranbrown1301@gmail.com>
2022-06-30 18:11:49 +0900

    styled cards img and p elements

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -22,7 +22,15 @@ function Popular() {
     <div>
       <Wrapper>
         <h3>Popular Picks</h3>
-        <Splide>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            gap: '5rem',
+          }}
+          >
           {popular.map((recipe) => {
             return(
               <SplideSlide>
@@ -47,9 +55,30 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow; hidden;
+  position: relative;
 
-  img{
+  img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
 
