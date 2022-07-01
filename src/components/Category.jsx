de8842457dc621303d499aae78aa01dd5f339edb
Keiran Brown <keiranbrown1301@gmail.com>
2022-07-01 17:28:06 +0900

    browser routes added in pages.jsx file

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,19 +6,19 @@ import{ NavLink } from 'react-router-dom'
 function Category() {
   return (
     <List>
-      <NavLink>
+      <NavLink to={'/cuisine/Italian'}>
         <FaPizzaSlice />
         <h4>Italian</h4>
       </NavLink>
-      <NavLink>
+      <NavLink to={'/cuisine/American'}>
         <FaHamburger />
         <h4>American</h4>
       </NavLink>
-      <NavLink>
+      <NavLink to={'/cuisine/Thai'}>
         <GiNoodles />
         <h4>Thai</h4>
       </NavLink>
-      <NavLink>
+      <NavLink to={'/cuisine/Japanese'}>
         <GiChopsticks />
         <h4>Japanese</h4>
       </NavLink>
