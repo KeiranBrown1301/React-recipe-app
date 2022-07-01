49e324b71b07aa5e1fcb538e56b7914aa162a440
Keiran Brown <keiranbrown1301@gmail.com>
2022-07-01 18:08:19 +0900

    added spoonacular complexSearch for specific cuisines.

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    )
+    const recipies = await data.json();
+    setCuisine(recipies.results)
+  }
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+
   return (
-    <div>Cuisine</div>
+    <div>
+
+    </div>
   )
 }
 
