d9dafcd4c4ff6265a8facb0644651234802763b0
Keiran Brown <keiranbrown1301@gmail.com>
2022-07-05 18:40:11 +0900

    styled the searchbar.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,16 +4,33 @@ import { FaSearch } from 'react-icons/fa';
 
 function Search() {
   return (
-    <form>
+    <FormStyle>
       <input type="text" />
-    </form>
+    </FormStyle>
     )
 }
 
-const Form = styled.form`
+const FormStyle = styled.form`
   margin: 0rem 20rem;
   position: relative;
   width: 100%;
+  input{
+    border: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.5rem;
+    color: white;
+    padding: 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+  }
+  svg{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
 `
 
 export default Search
